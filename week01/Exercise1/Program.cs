ca59187e4c93fae8a65c0b28184b295b0fe3d4cc
lillyniu <u696477693@byui.edu>
2025-01-14 20:54:09 -0700

    assignment 1 done

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        console.writeline("What is your first name?");
+        string first = console.readline();
+
+        console.writeline("What is your last name? ");
+        string last = console.readline();
+
+        console.writeline($"Your name is {first} {last}. ")
     }
 }
