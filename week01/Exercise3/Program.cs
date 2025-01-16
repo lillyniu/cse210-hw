4d41cfce874c9d8d7784edcc029133d0f7a761ab
lillyniu <u696477693@byui.edu>
2025-01-16 13:58:55 -0700

    Completed ass 3, moving on without checking

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+
+        do
+        {
+            Console.Write("Try to guess the magic number: ")
+            int guess = Console.ReadLine();
+
+            if (guess > number);
+                Console.WriteLine("lower")
+            else if(guess < number)
+            Console.WriteLine("higher")
+
+        } while (guess != number);
+        Console.WriteLine("Congrats! You guessed the magic number! ")
+    
+}
 }
