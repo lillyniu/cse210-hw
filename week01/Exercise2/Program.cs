3efefcde6f7cf589f436e3f328fe90a55892e856
lillyniu <u696477693@byui.edu>
2025-01-15 19:27:46 -0700

    Finished ass 2 week 1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What percentage did you get? ");
+        string valueFromUser = Console.ReadLine();
+        
+        int x = int.Parse(valueFromUser);
+
+        if (x > 90)
+        {
+            Console.WriteLine("You got an A. ");
+        }
+        else if (x > 80)
+        {
+            Console.WriteLine("You got an B. ");
+        }
+        else if (x > 70)
+        {
+            Console.WriteLine("You got an C. ");
+        }
+        else if (x > 60)
+        {
+            Console.WriteLine("You got an D. ");
+        }  
+        else if (x < 60)
+        {
+            Console.WriteLine("You got an F. ");
+        }
     }
 }
